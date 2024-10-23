c75835b234b9db31897e6e4f51a06f9b715bc5b4
Francisco <fvsalapic@gmail.com>
2024-10-23 13:07:06 +0200

    created ejs

--- web/routes/index.js
+++ web/routes/index.js
@@ -6,4 +6,16 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.get("/contact", function (req, res) {
+  res.render("contacto");
+});
+
+router.get("/home", function (req, res) {
+  res.render("home", { movies: movieList }); // 'movieList' es un array de películas
+});
+
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
 module.exports = router;
